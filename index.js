291f6f070adf25b1aeb64a7c08aa3539c152f344
hoangns99 <hoangns99.dev@gmail.com>
2024-07-19 11:04:11 +0700

    init project

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('', (req, res) => {
+    res.send('Hello Nodejs app')
+});
+
+app.listen(3000, async () => {
+    console.log(`Server is running in port 3000`);
+})
