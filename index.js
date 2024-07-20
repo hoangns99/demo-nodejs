4dfd2bb01f539fa8ec76a8bf3d4cd7a3e8155cea
hoangns99 <hoangns99.dev@gmail.com>
2024-07-20 10:34:17 +0700

    update for deploy

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('', (req, res) => {
     res.send('Hello Nodejs app')
 });
 
-app.listen(3000, async () => {
-    console.log(`Server is running in port 3000`);
+app.listen(8080, async () => {
+    console.log(`Server is running in port 8080`);
 })
